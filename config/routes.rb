dfe455b82772852683e88c9dd9013f7684411d9f
app2641 <app2641@gmail.com>
2015-11-02 00:22:44 +0900

    Set routing alias

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
   get '/tags'  => 'tag#list'
-  get '/:name' => 'tag#show', constraints: { name: /[^\/]+/ }
+  get '/:name' => 'tag#show', constraints: { name: /[^\/]+/ }, as: :entries
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
