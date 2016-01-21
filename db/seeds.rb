6399e0b4489ed664b66feadb785ff3b4d51583ca
app2641 <app2641@gmail.com>
2016-01-22 08:37:30 +0900

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@ puts 'loading tags....'
 
 if Tag.table_exists?
   tags = [
-    # ruby系
+    # Ruby系
     { id: 1, name: 'ruby', official_name: 'Ruby', is_active: true },
     { id: 2, name: 'rails', official_name: 'Ruby on Rails', is_active: true },
     { id: 3, name: 'sinatra', official_name: 'Sinatra', is_active: true },
@@ -20,7 +20,21 @@ if Tag.table_exists?
     { id: 6, name: 'java', official_name: 'Java', is_active: true },
     { id: 7, name: 'junit', official_name: 'JUnit', is_active: true },
     { id: 8, name: 'groovy', official_name: 'Groovy', is_active: true },
-    { id: 9, name: 'gradle', official_name: 'Gradle', is_active: true }
+    { id: 9, name: 'gradle', official_name: 'Gradle', is_active: true },
+
+    # PHP系
+    { id: 10, name: 'php', official_name: 'PHP', is_active: true },
+    { id: 11, name: 'symfony', official_name: 'Symfony', is_active: true },
+    { id: 12, name: 'cakephp', official_name: 'CakePHP', is_active: true },
+    { id: 13, name: 'zend', official_name: 'ZendFramework', is_active: true },
+    { id: 14, name: 'wordpress', official_name: 'WordPress', is_active: true },
+    { id: 15, name: 'doctrine', official_name: 'Doctrine', is_active: true },
+    { id: 16, name: 'phpunit', official_name: 'PHPUnit', is_active: true },
+    { id: 17, name: 'laravel', official_name: 'Laravel', is_active: true },
+    { id: 18, name: 'composer', official_name: 'Composer', is_active: true },
+    { id: 19, name: 'phpmyadmin', official_name: 'phpMyAdmin', is_active: true },
+
+    { id: 20, name: 'perl', official_name: 'Perl', is_active: true }
   ]
 
   tags.each do |t|
